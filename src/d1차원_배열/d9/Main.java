a31fb60936254505b34b30b7f75cf7b4de9da65e
TAEWOOKK <kto5294@gmail.com>
2024-08-19 22:48:51 +0900

    1차원 배열 9번 완료

---
+++ src/d1차원_배열/d9/Main.java
@@ -0,0 +1,48 @@
+package d1차원_배열.d9;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        int[] arr = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 1; i <= m; i++) {
+
+            int[] oriarr = arr;
+
+            stk = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(stk.nextToken());
+            int b = Integer.parseInt(stk.nextToken());
+
+            for (int j = a; j <= b; j++) {
+
+                arr[a] = oriarr[n-a];
+            }
+        }
+
+        br.close();
+
+        for (int i = 1; i <= n; i++) {
+
+            bw.write(arr[i]+" ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
