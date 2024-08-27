92509bb0bbeacf23d9c2e58b3b51b9f02d3fa82f
TAEWOOKK <kto5294@gmail.com>
2024-08-28 01:39:46 +0900

    심화1 6번 실패

--- src/f심화1/f6/Main.java
+++ src/f심화1/f6/Main.java
@@ -3,7 +3,6 @@ package f심화1.f6;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -21,16 +20,75 @@ public class Main {
 
         for (String word : words) {
 
+            List<String> exlist = new ArrayList<>();
 
+            for(String text : list){
 
+                int count = 0;
+                int index = 0;
+
+                if(!text.equals("dz=")){
+                    while ((index = text.indexOf(word, index)) != -1) {
+                        count++;
+                        index += word.length();
+                    }
+
+                    for (int i = 0; i < count; i++) {
+
+                        exlist.add(word);
+                    }
+
+                    String[] split = text.split(word);
+
+                    exlist.addAll(List.of(split));
+                }else{
+                    exlist.add(text);
+                }
+            }
+            list = exlist;
         }
 
-        String[] split = s.split("c=");
+        List<String> words1 = List.of(words);
+
+        for(int i = 0; i < list.size(); i++){
+
+            String s1 = list.get(i);
+
+            boolean equals = s1.equals("");
 
 
-        for(String word : split) {
-
-            System.out.println(word);
+            if (equals){
+                list.remove(i);
+            }
         }
+
+        List<String> dummy = new ArrayList<>();
+
+        int size = list.size();
+
+        for (int i = 0; i < size; i++) {
+
+            String word = list.get(i);
+
+            boolean contains = words1.contains(word);
+
+            if(!contains){
+
+                String[] split = word.split("");
+
+                list.addAll(List.of(split));
+                dummy.add(word);
+            }
+        }
+
+        for (String word : dummy) {
+
+            int i = list.indexOf(word);
+
+            list.remove(i);
+        }
+        bw.write(String.valueOf(list.size()));
+        bw.flush();
+        bw.close();
     }
 }
