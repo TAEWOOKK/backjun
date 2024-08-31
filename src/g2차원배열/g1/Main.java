985c9570ef7ed0d50af3a0d0bda1b0a0fe50c369
TAEWOOKK <kto5294@gmail.com>
2024-09-01 01:16:14 +0900

    2차원배열 1번 완료

--- src/g2차원배열/g1/Main.java
+++ src/g2차원배열/g1/Main.java
@@ -1,17 +1,38 @@
 package g2차원배열.g1;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    int[][] a = new int[3][3];
-    int[][] b = new int[3][3];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
+        int[][] a = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+
+                a[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+
+                bw.write(Integer.parseInt(st.nextToken())+a[i][j]+" ");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
 }
