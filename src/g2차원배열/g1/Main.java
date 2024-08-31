14b23334e4f3560b7462eb646c806380f624ab50
TAEWOOKK <kto5294@gmail.com>
2024-08-31 22:29:46 +0900

    2차원배열 1번 도전

---
+++ src/g2차원배열/g1/Main.java
@@ -0,0 +1,17 @@
+package g2차원배열.g1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int[][] a = new int[3][3];
+    int[][] b = new int[3][3];
+
+
+}
