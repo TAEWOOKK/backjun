85c5d46fd6870b8c2024ba8b6fdd12bdcf93c391
TAEWOOKK <kto5294@gmail.com>
2024-10-03 13:35:45 +0900

    기하 7번 진행중

---
+++ src/j기하직사각형과삼각형/j7/Main.java
@@ -0,0 +1,17 @@
+package j기하직사각형과삼각형.j7;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+        }
+    }
+}
