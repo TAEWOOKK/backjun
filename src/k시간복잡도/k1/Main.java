e8584917588870271e97fd5ddf999acc794d24d8
TAEWOOKK <kto5294@gmail.com>
2024-10-08 02:41:48 +0900

    시간복잡도 2번 완료

---
+++ src/k시간복잡도/k1/Main.java
@@ -0,0 +1,12 @@
+package k시간복잡도.k1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("1");
+        System.out.println("0");
+    }
+}
---
+++ src/k시간복잡도/k2/Main.java
@@ -0,0 +1,15 @@
+package k시간복잡도.k2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+
+        System.out.println(br.readLine());
+        System.out.println("1");
+    }
+}
