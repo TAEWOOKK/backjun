5fdd2411fe997bde509553f9fec2bf39fcf84d00
TAEWOOKK <kto5294@gmail.com>
2024-10-09 15:45:43 +0900

    시간복잡도 4번 완료

--- src/k시간복잡도/k4/Main.java
+++ src/k시간복잡도/k4/Main.java
@@ -1,24 +1,12 @@
 package k시간복잡도.k4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-
-        int count = 0; //수행 횟수를 저장하는 변수
-
-        for(int i = 1; i <= n-1; i++){
-            for(int j = i+1;  j <= n; j++){
-                count++;
-            }
-        }
-
-        System.out.println(count);
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n= sc.nextInt();
+        System.out.println((n*(n-1))/2);
+        System.out.println(2);
     }
 }
