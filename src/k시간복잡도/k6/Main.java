f110bb5f74f94cdbb61ef2df4eae9094cc123d91
TAEWOOKK <kto5294@gmail.com>
2024-10-11 20:07:37 +0900

    시간복잡도 6번 완료

--- src/k시간복잡도/k6/Main.java
+++ src/k시간복잡도/k6/Main.java
@@ -1,8 +1,17 @@
 package k시간복잡도.k6;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
 
+        Long n = Long.parseLong(br.readLine());
+
+        System.out.println( n*(n-1)*(n-2)/6);
+        System.out.println(3);
     }
 }
