239cca39b7cc0f693a694cb517e4520c44c7783c
TAEWOOKK <kto5294@gmail.com>
2024-10-13 18:50:18 +0900

    브루트포스1번 진행중

---
+++ src/l브루트포스/ㅣ1/Main.java
@@ -0,0 +1,22 @@
+package l브루트포스.ㅣ1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        String[] arr = br.readLine().split(" ");
+
+
+    }
+}
