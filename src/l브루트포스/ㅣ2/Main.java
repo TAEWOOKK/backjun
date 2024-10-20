e12bf0543d7ca919cdc12d2e22510482f450ca6a
TAEWOOKK <kto5294@gmail.com>
2024-10-20 21:48:29 +0900

    브루트 포스 2번 진행중

---
+++ src/l브루트포스/ㅣ2/Main.java
@@ -0,0 +1,13 @@
+package l브루트포스.ㅣ2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+
+    }
+}
