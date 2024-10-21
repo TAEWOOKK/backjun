7ea29965060988f71e190aead3fc1ea10e05bf2a
TAEWOOKK <kto5294@gmail.com>
2024-10-22 03:39:46 +0900

    브루트 포스 1번 완료

--- src/l브루트포스/ㅣ1/Main.java
+++ src/l브루트포스/ㅣ1/Main.java
@@ -3,6 +3,7 @@ package l브루트포스.ㅣ1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,8 +16,32 @@ public class Main {
         int n = Integer.parseInt(stk.nextToken());
         int m = Integer.parseInt(stk.nextToken());
 
-        String[] arr = br.readLine().split(" ");
+        String[] array = br.readLine().split(" ");
 
+        int[] arr = Arrays.stream(array)
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
+        int cha = 1000000;
+
+        int result = 0;
+
+        for (int i = 0; i < arr.length-2; i++) {
+            for (int j = i+1; j < arr.length-1; j++) {
+                for (int k = j+1; k < arr.length; k++) {
+
+                    int sum = arr[i] + arr[j] + arr[k];
+
+                    if(Math.abs(sum - m) < cha && sum <= m){
+
+                        result = sum;
+
+                        cha = Math.abs(sum - m);
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
     }
 }
