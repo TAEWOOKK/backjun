74b0be311bb96b8ac50e8aaaa8b4f0be6a921c94
TAEWOOKK <kto5294@gmail.com>
2024-11-02 19:07:28 +0900

    브루트 포스 5번 완료

---
+++ src/l브루트포스/I5/Main.java
@@ -0,0 +1,32 @@
+package l브루트포스.I5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int x = 0;
+        int count = 0;
+        while (true) {
+
+            String su = String.valueOf(x);
+
+
+            if(su.contains("666")){
+                count++;
+            }
+            if(count == n){
+                System.out.println(x);
+                break;
+            }else{
+                x++;
+            }
+        }
+    }
+}
